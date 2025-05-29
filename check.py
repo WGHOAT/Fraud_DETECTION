b0e404e9a07e0fb9e7a9216c68ec406ebb5240cb
WGHOAT <ajayraj180801@gmail.com>
2025-05-29 20:50:19 +0530

    Initial commit

---
+++ check.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+folderpath = "/home/ajay/Documents/sleeping_dog_don/FRAUD-DETECTION/dataset/data"
+pklfiles = [a for a in os.listdir(folderpath) if a.endswith(".pkl")]
+for f in pklfiles:
+    path = os.path.join(folderpath,f)
+    try:
+        files = pd.read_pickle(path)
+        print(f"{f}:type={type(files)}")
+    except Exception as e:
+        print(f"{f} Not loaded type={type(files)}")
